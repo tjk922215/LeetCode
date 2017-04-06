b2129b6f971e67f4139f61cfafe2e6ab846f6e76
tjk922215 <1351846321@qq.com>
2017-04-06 09:32:53 +0800

    change integer to roman

--- integer-to-roman.cpp
+++ integer-to-roman.cpp
@@ -1,33 +1,17 @@
-class Solution {
-public:
-    string intToRoman(int num) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
-        string roman = "IVXLCDM", res = "";
-        int div = 1000;
-        for(int i = 6; i >= 0; i -= 2) {
-            int tmp = num / div;
-            if(tmp == 0) {
-                div /= 10;
-                continue;
-            }
-            if(tmp <= 3) {
-                res.append(tmp, roman[i]);
-            } else if(tmp == 4) {
-                res.append(1, roman[i]);
-                res.append(1, roman[i+1]);
-            } else if(tmp == 5) {
-                res.append(1, roman[i+1]);
-            } else if(tmp <= 8) {
-                res.append(1, roman[i+1]);
-                res.append(tmp-5, roman[i]);
-            } else {
-                res.append(1, roman[i]);
-                res.append(1, roman[i+2]);
-            }
-            num %= div;
-            div /= 10;
-        }
-        return res;
-    }
-};
+class Solution {  
+public:  
+    string intToRoman(int num) {  
+        string str;    
+        string symbol[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};    
+        int value[]=    {1000,900,500,400, 100, 90,  50, 40,  10, 9,   5,  4,   1};   
+        for(int i=0;num!=0;++i)  
+        {  
+            while(num>=value[i])  
+            {  
+                num-=value[i];  
+                str+=symbol[i];  
+            }  
+        }  
+        return str;  
+    }  
+};  
