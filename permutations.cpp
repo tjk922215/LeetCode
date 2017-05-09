42aaa7d42930296ab5f88219cbc7e1d64dd4d733
tjk922215 <1351846321@qq.com>
2017-05-09 21:49:51 +0800

    change some code

--- permutations.cpp
+++ permutations.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-    int sz;
-    vector<vector<int> > vv;
-    void permutation(vector<int> &v, int left, int right) {
-        if(left == right) {
-            vv.push_back(v);
-            return;
-        }
-        permutation(v, left + 1, right);
-        for(int i = left + 1; i <= right; ++i) {
-            swap(v[left], v[i]);
-            permutation(v, left + 1, right);
-            swap(v[left], v[i]);
-        }
-    }
     vector<vector<int> > permute(vector<int> &num) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
-        vv.clear();
-        sz = num.size();
-        if(sz > 0)
-            permutation(num, 0, sz - 1);
-        return vv;
+        int length=num.size();
+        int k=fac(length);
+        vector<vector<int>> result;
+        sort(num.begin(),num.end());
+        for(int i=0;i < k; i++){
+            vector<int> visited(num);
+            vector<int> temp;
+            int p=i;
+            for(int j = length-1;j > 0;j --){
+                int m=p/fac(j);
+                temp.push_back(visited[m]);
+                visited.erase(visited.begin()+m);
+                p%=fac(j);
+            }
+            temp.push_back(visited[0]);
+            result.push_back(temp);
+        }
+        return result;
+    }
+private:
+    int fac(int n){
+        int num = 1;
+        for (int i = 1; i < n + 1; ++i)
+            num *= i;
+        return num;
     }
 };
